16cd5ec63f567d085c1ab9f7a60fc9b00258cd22
abe546 <abe546@live.com>
2020-07-02 02:07:02 -0400

    Add simple method for interpretting key press of W and key press of S

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,6 +1,27 @@
 //Sanity test :
 
-window.onload=function init()
-{//As soon as the window loads, the code in this scope will be executed.
-  console.log("Extension works to this degree");
-};
+$( document ).ready(main()); //Executed only once on page load, all other dynamic actions dictated by listener functions
+
+function main()
+{
+  //Main method
+  keyListener();
+}
+
+//Create function to interpret user key entry of 'W', and 'S'
+function keyListener()
+{
+  //w== 87, s== 83
+  $(document).keydown(function(key){
+
+   //w key is pressed
+   if(key.keyCode == 87)
+   {
+     console.log("The 'W' key was pressed");
+   }//s
+   else if(key.keyCode == 83)
+   {
+      console.log("The 'S' key was pressed");
+   }
+  });
+}
